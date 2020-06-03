363893b371c4bfae33296c6b78e2bfe28709ac94
karenahv <you@example.com>
2020-06-03 07:49:04 -0500

    log parsing corection

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
-"""Write a script that reads stdin line by line and computes metrics"""
+#!/usr/bin/python3
+"""check stdin"""
+
 
 import sys
+
 
 total_size = 0
 status = ['200', '301', '400', '401', '403', '404', '405', '500']
 times_status = [0, 0, 0, 0, 0, 0, 0, 0]
-count = 0
+counter = 0
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
@@ -17,13 +19,13 @@ try:
                 i = status.index(code)
                 times_status[i] += 1
             total_size += int(size)
-            count = count + 1
-        if count == 10:
+            counter += 1
+        if counter == 10:
             print("File size: {:d}".format(total_size))
             for i in range(8):
                 if times_status[i] != 0:
                     print("{:}: {:d}".format(status[i], times_status[i]))
-            count = 0
+            counter = 0
 except Exception:
     pass
 finally:
