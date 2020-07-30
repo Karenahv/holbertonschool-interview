2f31a351f1812860fc02531c48b52780aa9d22e8
Karenahv <837@holbertonschool.com>
2020-07-29 21:28:55 -0500

    n queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,48 +1,128 @@
 #!/usr/bin/python3
-""" puzzle queen challenge
+"""
+N Queens
 """
 
-from sys import argv, exit
+import sys
 
 
-def place(N, row, col, result):
-    """ place queens recursively """
-    while col < N:
-        if isvalid(row, col, result):
-            result.append([row, col])
-            if row == N-1:
-                print(result)
-                result.pop()
-            else:
-                place(N, row+1, 0, result)
-        col += 1
-    if len(result) > 0:
-        result.pop()
-    return
+def print_board(board):
+    """ print_board
+    Args:
+        board - list of list with length sys.argv[1]
+    """
+    new_list = []
+    for i, row in enumerate(board):
+        value = []
+        for j, col in enumerate(row):
+            if col == 1:
+                value.append(i)
+                value.append(j)
+        new_list.append(value)
+
+    print(new_list)
 
 
-def isvalid(row, col, result):
-    """ check if the position is valid """
-    diag1 = [l[0]+l[1] for l in result]
-    diag2 = [l[1]-l[0] for l in result]
-    cols = [l[1] for l in result]
-    rows = [l[0] for l in result]
-    if row in rows or col in cols or row+col in diag1 or col-row in diag2:
-        return False
+def isSafe(board, row, col, number):
+    """ isSafe
+    Args:
+        board - list of list with length sys.argv[1]
+        row - row to check if is safe doing a movement in this position
+        col - col to check if is safe doing a movement in this position
+        number: size of the board
+    Return: True of False
+    """
+
+    # Check this row in the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
     return True
 
 
-if __name__ == "__main__":
-    length = len(argv)
-    if length != 2:
+def solveNQUtil(board, col, number):
+    """ Auxiliar method to find the posibilities of answer
+    Args:
+        board - Board to resolve
+        col - Number of col
+        number - size of the board
+    Returns:
+        All the posibilites to solve the problem
+    """
+
+    if (col == number):
+        print_board(board)
+        return True
+        # print("hola")
+    #  if (col == number):
+    #     print_board(board)
+    #     return True
+
+    res = False
+    for i in range(number):
+
+        if (isSafe(board, i, col, number)):
+
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Make result true if any placement
+            # is possible
+            res = solveNQUtil(board, col + 1, number) or res
+
+            board[i][col] = 0  # BACKTRACK
+
+    return res
+
+
+def solve(number):
+    """ Find all the posibilities if exists
+    Args:
+        number - size of the board
+    """
+    board = [[0 for i in range(number)]for i in range(number)]
+
+    if not solveNQUtil(board, 0, number):
+        return False
+
+    return True
+
+
+def validate(args):
+    """ Validate the input data to verify if the size to
+        answer is posible
+    Args:
+        args - sys.argv
+    """
+    if (len(args) == 2):
+        # Validate data
+        try:
+            number = int(args[1])
+        except Exception:
+            print("N must be a number")
+            exit(1)
+        if number < 4:
+            print("N must be at least 4")
+            exit(1)
+        return number
+    else:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    N = int(argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-    result = []
-    place(N, 0, 0, result)
+
+
+if __name__ == "__main__":
+    """ Main method to execute the application
+    """
+
+    number = validate(sys.argv)
+    solve(number)
